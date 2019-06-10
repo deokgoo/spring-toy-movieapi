64ba8b2787cb635cd0f4727e2603563faa8eedce
deok <kkddgg1001@gmail.com>
2019-06-10 18:58:17 +0900

    ing...

--- src/main/java/com/deok/movieapi/controller/PageController.java
+++ src/main/java/com/deok/movieapi/controller/PageController.java
@@ -3,14 +3,16 @@ package com.deok.movieapi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
-@RequestMapping(value = "")
+@EnableWebMvc
+@RequestMapping("/")
 
 public class PageController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String something(){
-        return "index";
+        return "index.html";
     }
 }
